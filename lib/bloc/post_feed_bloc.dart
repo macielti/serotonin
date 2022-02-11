1ef35a4c5463e9da91dea74f3d121224f1b2d535
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-02-11 13:02:46 -0300

    use comumer provider from change notifier

--- lib/bloc/post_feed_bloc.dart
+++ lib/bloc/post_feed_bloc.dart
@@ -3,11 +3,12 @@ import 'package:serotonina/client/dolinho_client.dart';
 import 'package:serotonina/models/post.dart';
 
 class PostFeedBloc extends ChangeNotifier {
-  final bool loading = true;
+  late bool loading = true;
   late List<Post> posts = [];
 
   void fetchPosts() async {
     posts = await DolinhoClient().fetchNotCuratedPosts();
+    loading = false;
     notifyListeners();
   }
 }
--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:serotonina/bloc/post_feed_bloc.dart';
 import 'package:serotonina/client/dolinho_client.dart';
 import 'package:serotonina/component/loading_indicator.dart';
 import 'package:serotonina/component/player.dart';
-import 'package:serotonina/models/curatorship_stats.dart';
 import 'package:serotonina/models/post.dart';
 import 'package:video_player/video_player.dart';
 
@@ -13,32 +14,32 @@ class _PostFeedState extends State<PostFeed> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(_titleAppBar)),
-      body: FutureBuilder<List<Post>>(
-        future: DolinhoClient().fetchPosts(),
-        builder: (context, posts) {
-          if (posts.connectionState != ConnectionState.done) {
+      body: Consumer<PostFeedBloc>(
+        builder: (context, postFeedBloc, child) {
+          postFeedBloc.fetchPosts();
+          if (postFeedBloc.loading) {
             return LoadingIndicator();
           } else {
             return ListView.builder(
-              itemCount: posts.data!.length,
+              itemCount: postFeedBloc.posts.length,
               itemBuilder: (context, index) =>
-                  PostItem(post: posts.data![index]),
+                  PostItem(post: postFeedBloc.posts[index]),
             );
           }
         },
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          child: FutureBuilder<CuratorshipStats>(
-            future: DolinhoClient().fetchCuratorshipStats(),
-            builder: (context, curatorshipStats) {
-              return Text(
-                  '${curatorshipStats.data!.approvedTotalDurationSeconds.toString()}');
-            },
-          ),
-          height: 50.0,
-        ),
-      ),
+      // bottomNavigationBar: BottomAppBar(
+      //   child: Container(
+      //     child: FutureBuilder<CuratorshipStats>(
+      //       future: DolinhoClient().fetchCuratorshipStats(),
+      //       builder: (context, curatorshipStats) {
+      //         return Text(
+      //             '${curatorshipStats.data!.approvedTotalDurationSeconds.toString()}');
+      //       },
+      //     ),
+      //     height: 50.0,
+      //   ),
+      // ),
     );
   }
 }
