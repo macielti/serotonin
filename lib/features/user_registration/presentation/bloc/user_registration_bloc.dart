e59d230ac7ab4d48becb330610d0cf5f15253352
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-03-01 19:38:20 -0300

    wip add bloc for user registration feature

---
+++ lib/features/user_registration/presentation/bloc/user_registration_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class UserRegistrationBloc extends ChangeNotifier {
+  void submitNewUser({
+    required String username,
+    required String email,
+    required String password,
+  }) {
+
+  }
+}
