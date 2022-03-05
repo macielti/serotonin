f51b8d5c7bf63ffa4d6d3a6e55837b4dc9d3e281
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-03-05 05:16:44 -0300

    add tests for bloc

--- lib/features/user_registration/presentation/bloc/user_registration_bloc.dart
+++ lib/features/user_registration/presentation/bloc/user_registration_bloc.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:serotonina/features/user_registration/domain/usercases/submit_new_user.dart';
 
 class UserRegistrationBloc extends ChangeNotifier {
+  UserRegistrationBloc(this.submitNewUserUsecase);
+
+  bool isLoading = false;
+  SubmitNewUser submitNewUserUsecase;
 
   void submitNewUser({
     required String username,
     required String email,
     required String password,
-  }) {
-
+  }) async {
+    isLoading = true;
+    notifyListeners();
+    final params = Params(username: username, email: email, password: password);
+    await submitNewUserUsecase(params);
+    isLoading = false;
+    notifyListeners();
   }
 }
---
+++ test/features/user_registration/presentation/bloc/user_registration_bloc_test.dart
@@ -0,0 +1,58 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:serotonina/features/user_registration/domain/entities/User.dart';
+import 'package:serotonina/features/user_registration/domain/usercases/submit_new_user.dart';
+import 'package:serotonina/features/user_registration/presentation/bloc/user_registration_bloc.dart';
+
+import 'user_registration_bloc_test.mocks.dart';
+
+@GenerateMocks([SubmitNewUser])
+void main() {
+  MockSubmitNewUser? mockSubmitNewUserUsecase;
+  UserRegistrationBloc? bloc;
+
+  setUp(() {
+    mockSubmitNewUserUsecase = MockSubmitNewUser();
+    bloc = UserRegistrationBloc(mockSubmitNewUserUsecase!);
+  });
+
+  group('initial state', () {
+    test(
+      'when the bloc is instantiated it should be in a not loaded state',
+      () async {
+        expect(bloc!.isLoading, false);
+      },
+    );
+  });
+
+  group('submitNewUser', () {
+    final tUsername = 'admin';
+    final tEmail = 'admin@example.com';
+    final tPassword = 'very-strong-password';
+
+    final user =
+        User(id: 'random-uuid', username: tUsername, roles: [], email: tEmail);
+
+    test(
+      'when we the user submit a new user the bloc should act on top of that event',
+      () async {
+        when(mockSubmitNewUserUsecase!(any))
+            .thenAnswer((realInvocation) async => Right(user));
+
+        bloc!.submitNewUser(
+            email: tEmail, password: tPassword, username: tUsername);
+
+        await untilCalled(mockSubmitNewUserUsecase!(any));
+
+        verify(mockSubmitNewUserUsecase!(Params(
+          username: tUsername,
+          password: tPassword,
+          email: tEmail,
+        )));
+      },
+    );
+  });
+}
